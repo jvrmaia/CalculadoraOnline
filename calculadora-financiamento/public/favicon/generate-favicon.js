4e020cf807d971c5f5313af10f84b190215a24b2
João Víctor Rocon Maia <joao@joaovrmaia.com>
2025-03-03 08:23:07 -0300

    change favicon

---
+++ calculadora-financiamento/public/favicon/generate-favicon.js
@@ -0,0 +1,7 @@
+// This is a placeholder script
+// In a real-world scenario, you would use a tool like 'svg2png' and 'png2ico'
+// to convert the SVG to ICO format
+// For this example, we'll just use the SVG directly
+
+console.log('In a production environment, you would use a tool to convert SVG to ICO');
+console.log('For now, we are using the SVG directly as the favicon'); 
